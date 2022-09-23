a501c4af90314ee6304240fa73b47c76b99ca3d8
diego <bocosdiego@gmail.com>
2022-09-23 00:02:25 -0300

    inicie mi repo

---
+++ calcular.php
@@ -0,0 +1,12 @@
+
+<?php 
+
+$producto = $_POST['producto'];
+$precio = $_POST['precio'];
+$cantidad = $_POST['cantidad'];
+
+echo "<p> $producto</p>";
+
+echo "<p> $precio</p>";
+
+echo "<p> $cantidad</p>";
